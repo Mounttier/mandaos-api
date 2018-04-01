147b879c82f621274022df3188f9c38a59eb260e
Salvador Montiel <salvadormontiel8@gmail.com>
2018-03-31 22:39:15 -0600

    Removed useless files.

---
+++ src/test/java/com/smontiel/mandaos/MandaosApplicationTests.java
@@ -0,0 +1,17 @@
+package com.smontiel.mandaos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Salvador Montiel on 31/mar/2018.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MandaosApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+}
