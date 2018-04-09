e2303287649bf738edbcb5f4122e5595f7cdb6eb
Salvador Montiel <salvadormontiel8@gmail.com>
2018-04-09 17:17:13 -0500

    Make compatible with Heroku Postgres

--- src/main/java/com/smontiel/mandaos_api/AppConfiguration.java
+++ src/main/java/com/smontiel/mandaos_api/AppConfiguration.java
@@ -1,9 +1,13 @@
 package com.smontiel.mandaos_api;
 
 import com.smontiel.simple_jdbc.SimpleJDBC;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by Salvador Montiel on 22/mar/2018.
@@ -12,10 +16,19 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfiguration {
 
     @Bean
-    public SimpleJDBC simpleJDBC() {
-        String jdbcUrl = System.getenv("MANDAOS_POSTGRESQL_URL");
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setJdbcUrl(jdbcUrl);
+    public SimpleJDBC simpleJDBC() throws URISyntaxException {
+    	URI dbUri = new URI(System.getenv("MANDAOS_POSTGRESQL_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        HikariDataSource hikariDataSource = new HikariDataSource(config);
 
         return SimpleJDBC.from(hikariDataSource);
     }
